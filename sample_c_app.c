5085010567c52714ab68f8a42fe45a68d30047eb
hassnain360 <hasnain@bu.edu>
2020-10-13 08:20:34 +0500

    Just testing :)

---
+++ sample_c_app.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\n");
+    printf("This is a test run. Computers will one day rule the earth.");
+
+    int testInteger;
+    while(1)
+    {
+  
+    printf("Enter an integer: ");
+    scanf("%d", &testInteger);  
+    printf("Number = %d",testInteger);
+    
+    }
+
+    return 0;
+}
